0787a700666839f55c4457bbe196765e9e6e3f3b
Joseph Searle <josephwilliamsearle@gmail.com>
2023-09-02 21:06:26 +0100

    Cleaned up App.css and added new nav bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className='navbar'>
+          <p className='title'>Joseph Searle</p>
+          <p className='button'>Experience</p>
+          <p className='button'>Projects</p>
+          <p className='button'>Contact</p>
+        </div>
         <p>Personal Website</p>
       </header>
     </div>
