15f236ed4e3027c093ee6861c6e135aaf52b8717
Joseph Searle <josephwilliamsearle@gmail.com>
2023-09-02 22:38:56 +0100

    App.test.js passes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Personal Website/i);
+  const linkElement = screen.getByText(/Hi i'm Joseph/i);
   expect(linkElement).toBeInTheDocument();
 });
