c55609fc01126cf21850f4d425b7bb6525427ac5
Joseph Searle <josephwilliamsearle@gmail.com>
2023-09-12 09:18:36 +0100

    Navbar changes on screen width

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import '@fontsource/pacifico';
 import '@fontsource/roboto';
 import {useState, useEffect} from 'react';
 import WebNavbar from './components/WebNavbar';
+import MobileNavbar from './components/MobileNavbar';
 import coffee from './illustrations/coffee.png';
 import background from './illustrations/Background.png';
 import './App.css';
@@ -9,7 +10,7 @@ import './App.css';
 function App() {
   // Window Width variables
   const [width, setWidth] = useState(window.innerWidth);
-  const breakPoint = 620;
+  const breakPoint = 800;
 
   // Background styling
   const backgroundStyle={
@@ -22,16 +23,21 @@ function App() {
     window.addEventListener("resize", () => setWidth(window.innerWidth));
   }, []);
 
+  let navbar;
+  if (width > breakPoint) navbar = <WebNavbar/>
+  else navbar = <MobileNavbar/>
+
   return (
     <div className="App">
       <header className="App-header" style={backgroundStyle}>
-        <WebNavbar />
+        {navbar}
         <div className='cover'>
           <div className='cover-text-container'>
             <p className='text-1'>Hi i'm Joseph</p>
             <p className='text-2'>I'm a Software Developer</p>
           </div>
           <div className='cover-illustration'>
+            <p>{width}</p>
             <img src={coffee} alt='coffee.png'/>
           </div>
         </div>
---
+++ src/components/MobileNavbar.js
@@ -0,0 +1,12 @@
+import '../App.css';
+
+function MobileNavbar() {
+    return (
+        <div className='navbar'>
+          <p className='title'>Joseph Searle</p>
+          
+        </div>
+    );
+}
+
+export default MobileNavbar;
